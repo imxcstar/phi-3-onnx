8763277f2978e022a5502a07155ae4ee1d01a0a1
xcssa <imxcssa@qq.com>
2024-05-30 23:11:15 +0800

    新增gradio ui

---
+++ phi-3/AIChat.cs
@@ -0,0 +1,54 @@
+﻿using Gradio.Net;
+using Microsoft.ML.OnnxRuntimeGenAI;
+using System.Text;
+
+namespace phi3
+{
+    public class AIChat
+    {
+        private string _modelPath;
+
+        private Model _model;
+        private Tokenizer _tokenizer;
+
+        public AIChat(string modelPath)
+        {
+            _modelPath = modelPath;
+            _model = new Model(_modelPath);
+            _tokenizer = new Tokenizer(_model);
+        }
+
+        public async Task<string> ChatAsync(string message, IList<ChatbotMessagePair> chatHistory)
+        {
+            var chatHistoryStr = string.Join('\n', chatHistory.Select(x => @$"<|user|>
+{x.HumanMessage.TextMessage}<|end|>
+<|assistant|>
+{x.AiMessage.TextMessage}<|end|>
+"));
+            if (!string.IsNullOrWhiteSpace(chatHistoryStr))
+                chatHistoryStr += "\n";
+            string prompt = @$"{chatHistoryStr}<|user|>
+{message}<|end|>
+<|assistant|>
+";
+            var ret = new StringBuilder();
+            var sequences = _tokenizer.Encode(prompt);
+
+            using GeneratorParams generatorParams = new GeneratorParams(_model);
+            generatorParams.SetSearchOption("max_length", 200);
+            generatorParams.SetInputSequences(sequences);
+
+            using var tokenizerStream = _tokenizer.CreateStream();
+            using var generator = new Generator(_model, generatorParams);
+            while (!generator.IsDone())
+            {
+                generator.ComputeLogits();
+                generator.GenerateNextToken();
+                ret.Append(tokenizerStream.Decode(generator.GetSequence(0)[^1]));
+                await Task.Delay(1);
+            }
+
+            return ret.ToString();
+        }
+    }
+}
--- phi-3/Program.cs
+++ phi-3/Program.cs
@@ -1,35 +1,48 @@
-﻿using Microsoft.ML.OnnxRuntimeGenAI;
+﻿using Gradio.Net;
 
-Console.WriteLine("Loading...");
-
-//https://huggingface.co/microsoft/Phi-3-mini-4k-instruct-onnx/tree/main/cpu_and_mobile/cpu-int4-rtn-block-32-acc-level-4
-string modelPath = "./phi-3-mini/";
-
-using Model model = new Model(modelPath);
-using Tokenizer tokenizer = new Tokenizer(model);
-
-Console.Clear();
-while (true)
+namespace phi3
 {
-    Console.Write(">");
-    string prompt = @$"<|user|>
-{Console.ReadLine()}<|end|>
-<|assistant|>
-";
-    var sequences = tokenizer.Encode(prompt);
-
-    using GeneratorParams generatorParams = new GeneratorParams(model);
-    generatorParams.SetSearchOption("max_length", 200);
-    generatorParams.SetInputSequences(sequences);
-
-    using var tokenizerStream = tokenizer.CreateStream();
-    using var generator = new Generator(model, generatorParams);
-    while (!generator.IsDone())
+    public class Program
     {
-        generator.ComputeLogits();
-        generator.GenerateNextToken();
-        Console.Write(tokenizerStream.Decode(generator.GetSequence(0)[^1]));
-    }
+        //https://huggingface.co/microsoft/Phi-3-mini-4k-instruct-onnx/tree/main/cpu_and_mobile/cpu-int4-rtn-block-32-acc-level-4
+        static AIChat aiChat = new AIChat("./phi3-mini-4k-cpu");
 
-    Console.WriteLine();
+        static async Task Main(string[] args)
+        {
+            App.Launch(await CreateUI());
+        }
+
+        static async Task<Blocks> CreateUI()
+        {
+            using (var blocks = gr.Blocks())
+            {
+                gr.Markdown("phi-3-mini-4k-cpu");
+
+                var chatbot = gr.Chatbot();
+                var msg = gr.Textbox();
+
+                Button subBtn, clearBtn;
+
+                using (gr.Row())
+                {
+                    subBtn = gr.Button("Submit");
+                    clearBtn = gr.Button("Clear");
+                }
+
+                await subBtn.Click(fn: async input => await Respond(Textbox.Payload(input.Data[0]), Chatbot.Payload(input.Data[1])),
+                    inputs: [msg, chatbot], outputs: [msg, chatbot]);
+
+                await clearBtn.Click(fn: async input => gr.Output("", null),
+                    inputs: [msg, chatbot], outputs: [msg, chatbot]);
+
+                return blocks;
+            }
+        }
+
+        static async Task<Output> Respond(string message, IList<ChatbotMessagePair> chatHistory)
+        {
+            chatHistory.Add(new ChatbotMessagePair(message, await aiChat.ChatAsync(message, chatHistory)));
+            return gr.Output("", chatHistory);
+        }
+    }
 }
